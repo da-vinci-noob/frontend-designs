935528f960188115d1bcd31a337b01057868acfc
Shah Rukh <shah.xhtml@yahoo.com>
2022-12-23 02:02:31 +0530

    Create a Page to show all the Pages

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+const route = useRouter()
+const all_pages = ref([])
+
+all_pages.value = route.options.routes
+</script>
+
+<template>
+  <div>
+    <client-only>
+      <ol>
+        <div
+          v-for="page in all_pages.filter((obj) => obj.path !== '/')"
+          :key="page.path"
+        >
+          <div class="title-route">
+            <li>
+              <NuxtLink :to="page.path">
+                {{ page.name.replaceAll('-', ' ') }}
+              </NuxtLink>
+            </li>
+          </div>
+        </div>
+      </ol>
+    </client-only>
+  </div>
+</template>
+
+<style scoped>
+.title-route {
+  text-transform: capitalize;
+}
+</style>
